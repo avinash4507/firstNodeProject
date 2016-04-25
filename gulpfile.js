cbb6da3dc1bf0285d6a74884d156a7f995fef54b
avinash <avinash4507@gmail.com>
2016-04-26 02:10:58 +0530

    added ignorePath in gulpfile to remove ../../ from beginning of injected files

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,8 @@ gulp.task('inject', function() {
     var wiredep = require('wiredep').stream;
     var options = {
         bowerJson: require('./bower.json'),
-        directory: './public/lib'
+        directory: './public/lib',
+        ignorePath: '../../public'
     }
     
     return gulp.src('./src/views/*.html') // pulls in all html files and get us started
