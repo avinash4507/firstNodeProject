339afb5d0f52b08eda7a65b644f219d434110d23
avinash <avinash4507@gmail.com>
2016-04-25 22:38:14 +0530

    added express and used it for routhing and port listeniing
    Also added bootstrap template with a new folder structure embedded

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express'); //gives reference to express (no ability to do anything)
+
+var app = express(); //creates instance of express so that now we can do something with express
+
+var port = 5000;
+
+// app.use() allows us to setup some middleware
+// whatever is put in here is going to be used by express first before doing anything else
+// express.static(in here we setup a static directory by giving name)
+// anytime a request is made to css/styles.css, the first thing express does is look into the public directory and see if there is a static file in there and serves it
+// route same as that of static file after that static file wont do anything 
+app.use(express.static('public'))
+
+// function() is passed to express that tells it what to do
+// request is info coming from the browser
+// response is what we gonna deal with
+// express is taking request and giving back response
+app.get('/', function(request, response){
+    response.send('Hello Avinash')
+})  // slash('/') here represents home route (here localhost:5000)
+
+app.get('/books', function(req, res) {
+    res.send('Hello Books')
+})
+
+app.listen(port, function(err) {
+    console.log('Running on port '+port);
+});
